691979d9dfe8041d5962e82ab158ef6aec912864
AsulconS <rolandobedregal321@gmail.com>
2018-08-20 18:17:22 -0500

    These files have changed

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, my name is Adrian Bedregal. CUI: 20181741" << endl;
+    cout << "I'm editing the HelloWorld.cpp file" << endl;
     return 0;
 }
